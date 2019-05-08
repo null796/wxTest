6a3ea797e090c3ea12d532f9eb3cc117a967eb75
null796 <982254607@qq.com>
2019-05-08 17:57:32 +0800

    第八次作业

--- wxTest/components/Linklist/Linklist.js
+++ wxTest/components/Linklist/Linklist.js
@@ -11,7 +11,7 @@ Component({
     ItemList: [
       { "pagename": "WXML学习的基本语法", "pagetext": "/tools/Wxmls/wxml" },
       { "pagename": "利息计算器", "pagetext": "/tools/lxjsq/lxjsq" },
-      { "pagename": '计算器', "pagetext": "/tools/lxjsq/lxjsq" },
+      { "pagename": '计算器', "pagetext": "/tools/calc/calc" },
       { "pagename": '留言本', "pagetext": "/tools/lyb/lyb" },
       { "pagename": '课程表', "pagetext": "/tools/table/table" }, 
       { "pagename": '猜数游戏', "pagetext": "/tools/guess/guess" }, 
